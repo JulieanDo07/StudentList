7a85bb8e2b17b3c72ce789b249ddf1760ba2131e
julieando <julieando@localhost>
2023-01-24 11:01:11 -0500

    added getter and setter for address

--- src/student1/Student1.java
+++ src/student1/Student1.java
@@ -14,6 +14,15 @@ package student1;
 public class Student1 {
     private String studentId;
     private String studentName; 
+    private String address; 
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
     public Student1(String studentId, String studentName) {
         // using s-file name you don't need to use full name (e.g., sid or sname), in the parameters
